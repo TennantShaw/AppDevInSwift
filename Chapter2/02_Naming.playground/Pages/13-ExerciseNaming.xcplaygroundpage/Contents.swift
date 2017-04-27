0ad5cba88b35c5dc9ad4c617ce04530d080a4e70
Tennant Shaw <tennantshaw@Macs-MBP.lan>
2017-04-27 11:54:26 -0400

    initial commit

---
+++ Chapter2/02_Naming.playground/Pages/13-ExerciseNaming.xcplaygroundpage/Contents.swift
@@ -0,0 +1,43 @@
+/*:
+ ## Exercise: Good Names
+ 
+ You’ve inherited some code from another programmer, who never had a chance to take the _App Development with Swift_ course. All you found was a note with the following written on it:
+ 
+ > **Truck loading guide**
+ >
+ > Pallets of oranges weigh 100lbs
+ >
+ > Pallets of watermelons weigh 200lbs
+ >
+ > Have to load up the left then right of the truck and keep it as balanced. How many on each side? Melons always go together.
+*/
+let co = 14
+let cw = 3
+let ow = 100
+let ww = 200
+let to = co * ow
+let tw = cw * ww
+let ttl = to + tw
+let es = ttl / 2
+let lhso = es / ow
+let rhso = co - lhso
+
+
+//: - experiment: Rewrite the code so that it makes sense without needing a note. Use meaningful names and comments.
+let numberOfOrangeCases = 14
+let numberOfWatermelonCases = 3
+let orangeWeight = 100
+let watermelonWeight = 200
+let totalOrageWeight = numberOfOrangeCases * orangeWeight
+let totalWatermelonWeight = numberOfWatermelonCases * watermelonWeight
+let totalWeight = totalOrageWeight + totalWatermelonWeight
+let extraSpace = totalWeight / 2
+let leftSideSpace = extraSpace / orangeWeight
+let rightSideSpace = numberOfOrangeCases - leftSideSpace
+
+
+/*:
+ _Copyright (C) 2016 Apple Inc. All Rights Reserved.\
+ See LICENSE.txt for this sample’s licensing information._
+ */
+//:[Previous](@previous)  |  page 14 of 14
