4f155b7fc1edc7b4ce323c18048e2da2bfdaa76e
Tennant Shaw <tennantshaw@Macs-MBP.attlocal.net>
2017-04-29 21:22:02 -0400

    Finished Chapter 19

---
+++ Chapter19/19_EnumsAndSwitch.playground/Pages/Exercise-Replacing Bools.xcplaygroundpage/Contents.swift
@@ -0,0 +1,26 @@
+/*:
+ ## Exercise: Replacing Bools
+ 
+ The following struct describes a type of enemy in a game:
+*/
+
+struct Enemy {
+    let strength: Int
+    let speed: Int
+    let weapon: Weapons
+}
+/*: 
+ As your game has developed, you’ve decided that your enemies might have more than one type of weapon.
+ 
+ - callout(Exercise): Define an enum to represent the weapons an enemy might have: `none`, `sword`, `rubberMallet` and so on. Change the struct definition to use your new enum instead of a `Bool`.
+ 
+
+[Previous](@previous)  |  page 19 of 21  |  [Next: Exercise: Counting Votes](@next)
+*/
+enum Weapons {
+    case none
+    case sword
+    case rubberMallet
+    case axe
+    case spear
+}
